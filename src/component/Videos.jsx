8b3eb00a67820cef571296d21166cc280279d4ec
Your Name <you@example.com>
2023-11-05 09:16:11 -0800

    up

--- src/component/Videos.jsx
+++ src/component/Videos.jsx
@@ -1,8 +1,9 @@
 import { MoreVert } from "@mui/icons-material";
 import { Link as LinkRouter } from "react-router-dom";
-import img from '../assets/me.jpg'
+import img from "../assets/me.jpg";
 import {
   Avatar,
+  Box,
   Card,
   CardActionArea,
   CardContent,
@@ -10,25 +11,24 @@ import {
   Grid,
   IconButton,
   Link,
+  Skeleton,
   Stack,
   Tooltip,
   Typography,
 } from "@mui/material";
 import React, { useState } from "react";
 import MenuFN from "./MenuFN";
-export default function Videos({data}) {
+export default function Videos({ data }) {
+  console.log(data);
   const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl)
+  const open = Boolean(anchorEl);
   const HandelOpen = (e) => {
     setAnchorEl(e.currentTarget);
   };
-  return (
-    <Grid
-      container
-      sx={{ px: { md: 3, sm: 2, xs: 1 }, py: 1, mt: 11 }}
-      spacing={2}
-    >
-      {data.map((video) => (
+console.log(data);
+  const HandelVideosShow = () => {
+    if (data.length < 0) {
+      return data.map((video) => (
         <Grid item xs={12} sm={6} md={4} key={video.videoId}>
           <Card
             sx={{
@@ -37,12 +37,23 @@ export default function Videos({data}) {
               backgroundImage: "inherit",
             }}
           >
-            <CardActionArea LinkComponent={LinkRouter} sx={{display:"flex"}} to={`/video/${video.videoId}`}>
-              <CardMedia height={video.thumbnail[3].height} sx={{width:video.thumbnail[3].width}} src={video.thumbnail[3].url} component={"img"} />
+            <CardActionArea
+              LinkComponent={LinkRouter}
+              sx={{ display: "flex" }}
+              to={`/video/${video.videoId}`}
+            >
+              <CardMedia
+                height={video.thumbnail[3].height}
+                sx={{ width: video.thumbnail[3].width }}
+                src={video.thumbnail[3].url}
+                component={"img"}
+              />
             </CardActionArea>
             <CardContent sx={{ bgcolor: "#121212", pl: 0 }}>
               <Stack direction={"row"} spacing={1}>
-                <Avatar sx={{ bgcolor: "#5D4038", color: "white" }} src={img}>O</Avatar>
+                <Avatar sx={{ bgcolor: "#5D4038", color: "white" }} src={img}>
+                  O
+                </Avatar>
                 <Stack>
                   <IconButton
                     size="small"
@@ -59,7 +70,7 @@ export default function Videos({data}) {
                       },
                     }}
                   >
-                    <MoreVert fontSize="inherit"/>
+                    <MoreVert fontSize="inherit" />
                   </IconButton>
                   <Tooltip title={video.title} followCursor>
                     <Typography
@@ -75,14 +86,24 @@ export default function Videos({data}) {
                         textOverflow: "ellipsis",
                       }}
                     >
-                      <Link component={LinkRouter} to={`/video/${video.videoId}`} underline="none" color={"white"}>
+                      <Link
+                        component={LinkRouter}
+                        to={`/video/${video.videoId}`}
+                        underline="none"
+                        color={"white"}
+                      >
                         {video.title}
                       </Link>
                     </Typography>
                   </Tooltip>
                   <Tooltip title={video.channelTitle} followCursor>
                     <Typography variant="body2" component={"p"}>
-                      <Link component={LinkRouter} to={`/video/${video.videoId}`} underline="none" color={"text.secondary"}>
+                      <Link
+                        component={LinkRouter}
+                        to={`/video/${video.videoId}`}
+                        underline="none"
+                        color={"text.secondary"}
+                      >
                         {video.channelTitle}
                       </Link>
                     </Typography>
@@ -95,9 +116,32 @@ export default function Videos({data}) {
             </CardContent>
           </Card>
         </Grid>
-      ))}
+      ));
+    } else {
+      return Array.from(new Array(9).fill(null)).map((_, i) => (
+        <Grid item xs={12} sm={6} md={4}>
+          <Skeleton key={i} variant="rectangular" height={188}/>
+          <Stack direction={"row"} spacing={1} mt={1}>
+            <Skeleton variant="circular" width={50} height={50} />
+            <Box sx={{ pt: 0.5,width:"100%" }}>
+              <Skeleton />
+              <Skeleton width="60%" />
+            </Box>
+          </Stack>
+        </Grid>
+      ));
+    }
+  };
+
+  return (
+    <Grid
+      container
+      sx={{ px: { md: 3, sm: 2, xs: 1 }, py: 1, mt: 11 }}
+      spacing={2}
+    >
+      {HandelVideosShow()}
       {/* متواجده هنا حتي يتم تحسين الاداء لكي لا يتم تكرارها معهم وتفقد ادائها */}
-      <MenuFN anchorEl={anchorEl} setAnchorEl={setAnchorEl}/>
+      <MenuFN anchorEl={anchorEl} setAnchorEl={setAnchorEl} />
     </Grid>
   );
 }
